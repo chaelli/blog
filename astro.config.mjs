917ada4db385daaaa0be2a8d46ca675aba0f576c
Michael Keller <michael.keller@viu.ch>
2025-06-01 22:30:37 +0200

    update deploy workflow to support pushes to both master and main branches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,10 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
+import rehypeExternalLinks from "rehype-external-links";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://michael.keller.id",
